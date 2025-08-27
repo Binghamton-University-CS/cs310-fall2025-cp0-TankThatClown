e0d21b6b66b4e9d260d3898cf44ace299d1f7e46
hjiang13 <hjiang13@cs.binghamton.edu>
2025-08-26 21:07:45 -0400

    last change`

--- zoo.cpp
+++ zoo.cpp
@@ -6,6 +6,7 @@ using namespace std;
 int main() {
    Animal *animal1 = new Animal("African Elephant", 1758);
    Animal animal2("Giant Panda", 1869);
+   Animal *animal3= new Animal("Silverback Gorilla",1828);
    AnimalsInZoo *zooAnimals1= new AnimalsInZoo(animal2);
   AnimalsInZoo *zooAnimals2 = new AnimalsInZoo();
    delete animal1;
@@ -13,6 +14,7 @@ int main() {
 
    animal2.display();
    animal1->display();
+   animal3->display();
    cout << "With object passed" <<endl;
    zooAnimals1->display();
    cout << "No object passed" << endl;
